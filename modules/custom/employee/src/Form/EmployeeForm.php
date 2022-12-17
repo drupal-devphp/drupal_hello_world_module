ca9bd77b577d655256c0a7110a2e9f39e1c141aa
devphp <contact.devphp@gmail.com>
2022-12-18 05:00:29 +0530

    Added validations for form

--- modules/custom/employee/src/Form/EmployeeForm.php
+++ modules/custom/employee/src/Form/EmployeeForm.php
@@ -66,6 +66,29 @@ class EmployeeForm extends FormBase {
    * {@inheritdoc}
    */
   public function validateForm(array &$form, FormStateInterface $form_state) {
+    $formField = $form_state->getValues();
+
+    $firstName = trim($formField['emp_firstname']);
+    $lastName = trim($formField['emp_lastname']);
+    $email = trim($formField['emp_email']);
+    $zipcode = trim($formField['emp_zipcode']);
+
+    if (!preg_match("/^([a-zA-Z']+)$/", $firstName)) {
+      $form_state->setErrorByName('emp_firstname', $this->t('Enter the valid first name'));
+    }
+
+    if (!preg_match("/^([a-zA-Z']+)$/", $lastName)) {
+      $form_state->setErrorByName('emp_lastname', $this->t('Enter the valid last name'));
+    }
+
+    if (!\Drupal::service('email.validator')->isValid($email)) {
+      $form_state->setErrorByName('emp_email', $this->t('Enter valid email address'));
+    }
+
+    if (!preg_match("/^\d{1,6}$/", $zipcode)) {
+      $form_state->setErrorByName('emp_zipcode', $this->t('Enter the valid zip code'));
+    }
+
   }
 
   /**
@@ -73,7 +96,7 @@ class EmployeeForm extends FormBase {
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
     $this->messenger()->addStatus($this->t('Employee data has been saved successsfully.'));
-    $form_state->setRedirect('<front>');
+    $form_state->setRedirect('employee.employee');
   }
 
 }
